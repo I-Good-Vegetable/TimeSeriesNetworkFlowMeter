4a3010cc054d93310562310b328ef3d74c4c584f
Jinxin Liu <jinliu@partner.ciena.com>
2022-08-30 17:17:12 -0400

    Avoid repeatedly read config

--- TimeSeriesNetworkFlowMeter/Config.py
+++ TimeSeriesNetworkFlowMeter/Config.py
@@ -4,8 +4,13 @@ import py
 
 import TimeSeriesNetworkFlowMeter
 
+
 CONFIG_PATH = os.path.join(os.path.dirname(TimeSeriesNetworkFlowMeter.__file__), 'Config.ini')
 
 
+_config = py.iniconfig.IniConfig(CONFIG_PATH)
+print(f'Config has been loaded')
+
+
 def getConfig():
-    return py.iniconfig.IniConfig(CONFIG_PATH)
+    return _config
